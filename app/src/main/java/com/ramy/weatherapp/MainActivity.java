7cd093df21f0a88fd6a92b365ed3cb3da66c032d
kramlj00 <73222835+kramlj00@users.noreply.github.com>
2021-04-08 18:48:39 +0200

    Parse JSON

--- app/src/main/java/com/ramy/weatherapp/MainActivity.java
+++ app/src/main/java/com/ramy/weatherapp/MainActivity.java
@@ -146,6 +146,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response){
                 Log.d("Clima", "Success! JSON: " + response.toString());
+
+                WeatherDataModel weatherDataModel = WeatherDataModel.fromJson(response);
+
             }
 
             @Override
