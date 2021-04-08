f55b0e7fc830a0a2a5564c90595c9b98ad874390
kramlj00 <73222835+kramlj00@users.noreply.github.com>
2021-04-08 18:11:57 +0200

    Linking the elements in the layout to Java code

--- app/src/main/java/com/ramy/weatherapp/MainActivity.java
+++ app/src/main/java/com/ramy/weatherapp/MainActivity.java
@@ -3,12 +3,26 @@ package com.ramy.weatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Member Variables:
+    TextView mCityLabel;
+    ImageView mWeatherImage;
+    TextView mTemperatureLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Linking the elements in the layout to Java code
+        mCityLabel = (TextView) findViewById(R.id.locationTV);
+        mWeatherImage = (ImageView) findViewById(R.id.weatherSymbolIV);
+        mTemperatureLabel = (TextView) findViewById(R.id.tempTV);
+        ImageButton changeCityButton = (ImageButton) findViewById(R.id.changeCityButton);
     }
 }
